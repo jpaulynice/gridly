738014a6585b24d21d34ad926ec371d9c25232cf
julesbond007 <jay.paulynice@gmail.com>
2014-05-25 19:52:12 -0400

    changes

--- js/app/views/gridRow.js
+++ js/app/views/gridRow.js
@@ -12,44 +12,44 @@ define([ 'jquery',
 		template : gridRowTemplate,
 		tagName : "tr",
 		attributes:{
-		draggable:true
-	},
-
-	events: {
-		"dragstart": "start",
-		"dragenter": "enter",
-		"dragleave": "leave",
-		"dragend": "leave",
-		"dragover": "over",
-		"drop": "drop"
-	},
-
-	start: function () {
-		this.model.collection.draggedModel = this.model;
-	},
-
-	enter: function () {
-		this.$el.addClass('drop-over');
-	},
-
-	leave: function () {
-		this.$el.removeClass('drop-over');
-	},
-
-	over: function (event) {
-		event.preventDefault();
-	},
-
-	drop: function () {
-		this.leave();
-		var collection = this.model.collection;
-		collection.models.splice(collection.indexOf(this.model.collection.draggedModel), 1);
-		collection.models.splice(this.$el.index(), 0, this.model.collection.draggedModel);
-		_.forEach(collection.models, function (model, index) {
-			model.set(this.orderField, index+1, {silent: true});
-		}, this);
-		collection.trigger("change");
-	}
+			draggable:true
+		},
+	
+		events: {
+			"dragstart": "start",
+			"dragenter": "enter",
+			"dragleave": "leave",
+			"dragend": "leave",
+			"dragover": "over",
+			"drop": "drop"
+		},
+	
+		start: function () {
+			this.model.collection.draggedModel = this.model;
+		},
+	
+		enter: function () {
+			this.$el.addClass('drop-over');
+		},
+	
+		leave: function () {
+			this.$el.removeClass('drop-over');
+		},
+	
+		over: function (event) {
+			event.preventDefault();
+		},
+	
+		drop: function () {
+			this.leave();
+			var collection = this.model.collection;
+			collection.models.splice(collection.indexOf(this.model.collection.draggedModel), 1);
+			collection.models.splice(this.$el.index(), 0, this.model.collection.draggedModel);
+			_.forEach(collection.models, function (model, index) {
+				model.set(this.orderField, index+1, {silent: true});
+			}, this);
+			collection.trigger("change");
+		}
 	});
 
 	return GridRow;
