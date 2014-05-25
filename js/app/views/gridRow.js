e37d3124f76bca35e742579764f17d349c20b165
julesbond007 <jay.paulynice@gmail.com>
2014-05-25 19:23:03 -0400

    drag and drop works now.

--- js/app/views/gridRow.js
+++ js/app/views/gridRow.js
@@ -11,8 +11,49 @@ define([ 'jquery',
 	var GridRow = Backbone.Marionette.ItemView.extend({
 		template : gridRowTemplate,
 		tagName : "tr",
-	    attributes:{
-	      draggable:true
+		attributes:{
+			draggable:true
+		},
+
+		events: {
+			"dragstart": "start",
+        	"dragenter": "enter",
+        	"dragleave": "leave",
+        	"dragend": "leave",
+        	"dragover": "over",
+        	"drop": "drop"
+		},
+
+		start: function () {
+			debugger;
+        	this.parent.draggedModel = this.model;
+	    },
+
+	    enter: function () {
+	    	debugger;
+	        this.$el.addClass(this.overClass);
+	    },
+
+	    leave: function () {
+	    	debugger;
+	        this.$el.removeClass(this.overClass);
+	    },
+
+	    over: function (event) {
+	    	debugger;
+	        event.preventDefault();
+	    },
+
+	    drop: function () {
+	    	debugger;
+	        this.leave();
+	        var collection = this.parent.model;
+	        collection.models.splice(collection.indexOf(this.parent.draggedModel), 1);
+	        collection.models.splice(this.$el.index(), 0, this.parent.draggedModel);
+	        _.forEach(collection.models, function (model, index) {
+	            model.set(this.orderField, index+1, {silent: true});
+	        }, this);
+	        collection.trigger("change");
 	    }
 	});
 
