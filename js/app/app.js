7c7f05ab6f57fdb68753261e45cb994a32ba9e07
Jules Jay Paulynice <julesbond007@users.noreply.github.com>
2014-09-09 21:59:23 -0400

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -14,18 +14,18 @@ define([ 'jquery',
 		gridRegion: '.gridRegion'
 	})
 
-	var data = {};
-	data.rows = [
-	    {prefix:'Mr.',first:'Harry',last:'Potter',gender:'m',height:70,weight:140,age:19,has_nose:'True'},
-	    {prefix:'Ms.',first:'Hermione',last:'Granger',gender:'f',height:66,weight:121,age:19,has_nose:'True'},
-	    {prefix:'NULL',first:'Dobby',last:'NULL',gender:'m',height:23,weight:20,age:284,has_nose:'True'},
-	    {prefix:'Ms.',first:'Luna',last:'Lovegood',gender:'f',height:68,weight:99,age:17,has_nose:'True'},
-	    {prefix:'NULL',first:'Hagrid',last:'NULL',gender:'m',height:108,weight:553,age:45,has_nose:'True'},
-	    {prefix:'Dark Lord',first:'Tom',last:'Riddle',gender:'m',height:74,weight:144,age:48,has_nose:'False'},
-	    {prefix:'"Moaning"',first:'Myrtle',last:'NULL',gender:'f',height:65,weight:0,age:106,has_nose:'NULL'}];
-
-	data.gridRegion = App.gridRegion;
-
+	var data = {
+		gridRegion: App.gridRegion,
+		rows = [{
+			
+		    	prefix:'Mr.',first:'Harry',last:'Potter',gender:'m',height:70,weight:140,age:19,has_nose:'True'},
+		    {prefix:'Ms.',first:'Hermione',last:'Granger',gender:'f',height:66,weight:121,age:19,has_nose:'True'},
+		    {prefix:'NULL',first:'Dobby',last:'NULL',gender:'m',height:23,weight:20,age:284,has_nose:'True'},
+		    {prefix:'Ms.',first:'Luna',last:'Lovegood',gender:'f',height:68,weight:99,age:17,has_nose:'True'},
+		    {prefix:'NULL',first:'Hagrid',last:'NULL',gender:'m',height:108,weight:553,age:45,has_nose:'True'},
+		    {prefix:'Dark Lord',first:'Tom',last:'Riddle',gender:'m',height:74,weight:144,age:48,has_nose:'False'},
+		    {prefix:'"Moaning"',first:'Myrtle',last:'NULL',gender:'f',height:65,weight:0,age:106,has_nose:'NULL'}]
+	};
 	var gridController = new GridController(data);
 
 	return App;
