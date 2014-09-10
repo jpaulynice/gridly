a52713b0af7a797197bd6d21fc98106cea41111b
julesbond007 <jay.paulynice@gmail.com>
2014-09-10 08:09:43 -0400

    refactor

--- js/app/app.js
+++ js/app/app.js
@@ -1,32 +1,87 @@
 /**
  * Modularized Backbone.Marionette Application
  */
-define([ 'jquery',
-         'underscore',
-         'backbone',
-         'marionette',
-         'app/controllers/gridController' ],
-         function($, _, Backbone, Marionette,GridController) {
+define(['jquery', 
+        'underscore', 
+        'backbone', 
+        'marionette', 
+        'app/controllers/gridController'], 
+        function($, _, Backbone, Marionette, GridController) {
 
-	var App = new Backbone.Marionette.Application();
+    var App = new Backbone.Marionette.Application();
 
-	App.addRegions({
-		gridRegion: '.gridRegion'
-	})
+    App.addRegions({
+        gridRegion : '.gridRegion'
+    });
 
-	var data = {
-		gridRegion: App.gridRegion,
-		rows = [{
-			
-		    	prefix:'Mr.',first:'Harry',last:'Potter',gender:'m',height:70,weight:140,age:19,has_nose:'True'},
-		    {prefix:'Ms.',first:'Hermione',last:'Granger',gender:'f',height:66,weight:121,age:19,has_nose:'True'},
-		    {prefix:'NULL',first:'Dobby',last:'NULL',gender:'m',height:23,weight:20,age:284,has_nose:'True'},
-		    {prefix:'Ms.',first:'Luna',last:'Lovegood',gender:'f',height:68,weight:99,age:17,has_nose:'True'},
-		    {prefix:'NULL',first:'Hagrid',last:'NULL',gender:'m',height:108,weight:553,age:45,has_nose:'True'},
-		    {prefix:'Dark Lord',first:'Tom',last:'Riddle',gender:'m',height:74,weight:144,age:48,has_nose:'False'},
-		    {prefix:'"Moaning"',first:'Myrtle',last:'NULL',gender:'f',height:65,weight:0,age:106,has_nose:'NULL'}]
-	};
-	var gridController = new GridController(data);
+    var data = {
+        gridRegion : App.gridRegion,
+        rows : [{
+            prefix : 'Mr.',
+            first : 'Harry',
+            last : 'Potter',
+            gender : 'm',
+            height : 70,
+            weight : 140,
+            age : 19,
+            has_nose : 'True'
+        }, {
+            prefix : 'Ms.',
+            first : 'Hermione',
+            last : 'Granger',
+            gender : 'f',
+            height : 66,
+            weight : 121,
+            age : 19,
+            has_nose : 'True'
+        }, {
+            prefix : 'NULL',
+            first : 'Dobby',
+            last : 'NULL',
+            gender : 'm',
+            height : 23,
+            weight : 20,
+            age : 284,
+            has_nose : 'True'
+        }, {
+            prefix : 'Ms.',
+            first : 'Luna',
+            last : 'Lovegood',
+            gender : 'f',
+            height : 68,
+            weight : 99,
+            age : 17,
+            has_nose : 'True'
+        }, {
+            prefix : 'NULL',
+            first : 'Hagrid',
+            last : 'NULL',
+            gender : 'm',
+            height : 108,
+            weight : 553,
+            age : 45,
+            has_nose : 'True'
+        }, {
+            prefix : 'Dark Lord',
+            first : 'Tom',
+            last : 'Riddle',
+            gender : 'm',
+            height : 74,
+            weight : 144,
+            age : 48,
+            has_nose : 'False'
+        }, {
+            prefix : '"Moaning"',
+            first : 'Myrtle',
+            last : 'NULL',
+            gender : 'f',
+            height : 65,
+            weight : 0,
+            age : 106,
+            has_nose : 'NULL'
+        }]
+    };
+    var gridController = new GridController(data);
 
-	return App;
+    return App;
 });
